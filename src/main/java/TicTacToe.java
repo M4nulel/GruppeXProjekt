92eefa45918d780ddf1f50bc47e8baa26a4b5b79
Philipp <phiwim@gmail.com>
2026-06-11 12:31:11 +0200

    Implement board printing logic and game initialization

---
+++ src/main/java/TicTacToe.java
@@ -0,0 +1,23 @@
+public class TicTacToe {
+    private Player player1;
+    private Player player2;
+    private Player currentPlayer;
+    private Board board;
+
+    public TicTacToe() {
+        player1 = new Player('X');
+        player2 = new Player('O');
+        currentPlayer = player1;
+        board = new Board();
+    }
+
+    public void start() {
+        System.out.println("Current Player: " + currentPlayer.getMarker());
+        board.print();
+    }
+
+    public static void main(String[] args) {
+        TicTacToe game = new TicTacToe();
+        game.start();
+    }
+}
